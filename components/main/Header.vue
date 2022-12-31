b7d464afa099731363989caadc9787d3572723ef
arunstop <arunstop@gmail.com>
2022-12-31 19:50:30 +0700

    added header

---
+++ components/main/Header.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+  import { IHeaderItem } from './HeaderMenu.vue'
+
+  const menus: IHeaderItem[] = [
+    { label: 'Contact', link: '#contact' },
+    { label: 'Service', link: '#service' },
+    { label: 'Skills', link: '#skills' },
+    { label: 'Projects', link: '#projects' },
+    { label: 'Portfolio', link: '#projects' },
+  ]
+</script>
+<template>
+  <nav class="fixed top-0 z-[1] hidden max-sm:flex">
+    <CommonButton
+      class="m-2 flex aspect-square h-9 !min-w-0 items-center justify-center rounded-full bg-black/20 !bg-none p-2"
+    >
+      <Icon name="mdi:menu" class="text-xl" />
+    </CommonButton>
+  </nav>
+  <nav class="sticky top-0 z-[1] hidden w-full bg-zinc-500/50 p-2 sm:flex sm:p-4">
+    <div class="flex w-full gap-2 sm:gap-4">
+      <div class="">Arunseto Pambudi</div>
+      <div class="grow"></div>
+      <MainHeaderMenu v-for="(item, idx) in menus" :key="idx" v-bind="item" />
+    </div>
+  </nav>
+</template>
---
+++ components/main/HeaderMenu.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+  export interface IHeaderItem {
+    label: string
+    link: string
+    action?: () => void
+  }
+  defineProps<IHeaderItem>()
+</script>
+<template>
+  <NuxtLink
+    :to="link"
+    class="flex font-bold transition-all hover:-translate-y-2 transition-all duration-500 ease-in-out"
+  >
+    <span>
+      {{ label }}
+    </span>
+  </NuxtLink>
+</template>
